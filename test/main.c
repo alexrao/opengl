67b69cf6aa7026b8e821455defc432ada0b6e268
alexrao <rxp2013@163.com>
2016-03-16 13:54:51 +0800

    修改为输出正选波

--- test/main.c
+++ test/main.c
@@ -3,58 +3,29 @@
 #include <math.h>
 #include <stdio.h>
 
-const GLfloat Pi = 3.1415926f;
-//void display(void)
-//{
-//    int i;
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    //glRectf(-0.5f, -0.5f, 0.8f, 0.8f);
-//
-//    glBegin(GL_POINTS);
-//
-//    glVertex2f(0, 0);
-//
-//    for(i=0; i<n; i++)
-//    {
-//        //glVertex2f(R*cos(2*Pi/n*i), R*sin(2*Pi/n*i));
-//        glVertex2f(R*cos(2*Pi/n*i), R*sin(2*Pi/n*i));
-//        //glVertex2f(R*cos(angle), R/n*i);
-//    }
-//    glVertex2f(R*cos(0), R*sin(0));
-//    glEnd();
-//
-//    // Display i
-//    glFlush();
-//
-//}
-
+const GLfloat factor = 0.05f;
 
 void display(void)
 {
-    GLfloat a = 1/(2-2*cos(72*Pi/180));
-    GLfloat bx = a *cos(18*Pi/180);
-    GLfloat by = a *sin(18*Pi/180);
-
-    GLfloat cy = -a *sin(18*Pi/180);
-
-    GLfloat PointA[2] = {0, a},
-            PointB[2] = {bx, by},
-            PointC[2] = {0.5, cy},
-            PointD[2] = {-0.5, cy},
-            PointE[2] = {-bx, by};
-
 
     glClear(GL_COLOR_BUFFER_BIT);
-    //glRectf(-0.5f, -0.5f, 0.8f, 0.8f);
 
-    glBegin(GL_LINE_LOOP);
+    glBegin(GL_LINES);
 
-    glVertex2fv(PointA);
-    glVertex2fv(PointC);
-    glVertex2fv(PointE);
-    glVertex2fv(PointB);
-    glVertex2fv(PointD);
+    // x 轴
+    glVertex2f(-1.0f, 0.0f);
+    glVertex2f(1.0f, 0.0f);
+    // y 轴
+    glVertex2f(0.0f, -1.0f);
+    glVertex2f(0.0f, 1.0f);
 
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    for(GLfloat x=-1.0f/factor; x<1.0f/factor; x += 0.01f)
+    {
+        glVertex2f(x*factor, 5*sin(x)*factor);
+    }
     glEnd();
 
     // Display i
@@ -75,7 +46,7 @@ int main(int argc, char **argv)
     glutInitWindowPosition(100, 100);
 
     // Configure Window Szie
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(700, 400);
 
     // Create Window
     glutCreateWindow("OpenGL Test");
